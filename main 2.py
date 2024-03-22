44030cbd6a6897987c43b2e45fc2a7b1b528c1cc
Roman <Ramenm1001@gmail.com>
2024-03-22 22:56:33 +1000

    123

---
+++ main 2.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self):
+        self.hp = 100
+
+
+player = Player()
+player.hp -= 5
+
+
+class Player:
+    def __init__(self):
+        self.hp = 100
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+
+
+player = Player()
+player.take_damage(5)
